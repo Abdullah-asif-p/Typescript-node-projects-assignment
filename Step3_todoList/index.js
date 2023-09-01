d64be404b8ca4485b80190ce32b3c3dd8976e35e
Abdullah <abdullahasifmf007+github@gmail.com>
2023-09-01 11:02:03 +0500

    First commit

---
+++ Step3_todoList/index.js
@@ -0,0 +1,79 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const sleep = (ms = 2500) => new Promise((r) => setTimeout(r, ms));
+const startapp = async () => {
+    console.clear();
+    console.log(`
+  ${chalk.bgBlue("Welcome to the TODO-List App! This app helps you manage your tasks efficiently")}\n
+  ${chalk.bold("Instructions:")}\n
+  1. Enter your tasks one by one. Press Enter after each task.\n
+  2. To finish entering tasks, simply press Enter without typing anything.\n
+  3. Your entered tasks will be displayed.\n
+  4. If no tasks are entered the program will be terminated
+  Get organized and stay productive with this simple TODO-list app!\n
+  `);
+    await sleep();
+    userInput();
+};
+const userInput = async () => {
+    let userData = [];
+    let flag = false;
+    while (flag == false) {
+        const input = await inquirer.prompt([
+            {
+                message: "Todos:",
+                type: "input",
+                name: "to",
+            },
+        ]);
+        let todos = input.to;
+        if (todos == "") {
+            flag = true;
+        }
+        else {
+            flag = false;
+            userData.push(todos);
+        }
+    }
+    if (userData.length == 0) {
+        process.exit(0);
+    }
+    else {
+        return operation(userData);
+    }
+};
+const deleteTask = (userData, index) => {
+    userData.splice(index - 1, 1);
+    return userData;
+};
+const operation = async (userData) => {
+    const op = await inquirer.prompt([
+        {
+            message: "Do want to Delete any tasks in todo list?",
+            type: "list",
+            name: "choice",
+            choices: ["Yes", "No"],
+        },
+    ]);
+    if (op.choice == "Yes") {
+        const index = await inquirer.prompt([
+            {
+                name: "i",
+                type: "number",
+                message: `Enter the task number 1-${userData.length}`,
+            },
+        ]);
+        const data = deleteTask(userData, index.i);
+        output(data);
+    }
+    else {
+        output(userData);
+    }
+};
+const output = (userData) => {
+    userData.forEach((tasks, index) => {
+        console.log(`${index + 1}.${tasks}`);
+    });
+};
+await startapp();
