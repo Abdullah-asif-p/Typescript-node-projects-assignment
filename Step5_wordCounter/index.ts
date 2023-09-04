be6a1e7c0e5ed3f6c6b2dd7a5b8e189243061d58
Abdullah <abdullahasifmf007+github@gmail.com>
2023-09-04 11:51:21 +0500

    First commit

---
+++ Step5_wordCounter/index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+const startapp = async () => {
+  console.clear();
+  console.log(`${chalk.yellowBright("\t Welcome to the Text Analysis App!")}\n
+This app helps you analyze text input by counting the number of words and characters in it.
+  `);
+  await sleep();
+  await userInput();
+};
+const userInput = async () => {
+  const input = await inquirer.prompt({
+    type: "input",
+    name: "user",
+    message: "Enter your text",
+  });
+  const userData: string = String(input.user).trim();
+  output(userData);
+};
+
+const output = (Data: string) => {
+  let words = Data.split(" ");
+  let str = Data.replace(/\s/g, "");
+  console.log(
+    `text contains ${chalk.yellowBright(
+      words.length
+    )} words and ${chalk.yellowBright(str.length)} characters`
+  );
+};
+startapp();
